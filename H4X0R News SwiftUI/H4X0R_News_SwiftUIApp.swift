036913a1b91cc4a31da4b1c76f3a00295cabc230
gog1na <goginashvili.giorgi.gg@gmail.com>
2023-11-27 11:57:21 +0400

    Initial commit

---
+++ H4X0R News SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R News SwiftUI
+//
+//  Created by Giorgi Goginashvili on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
